ff7058b970abc0bfe06b3ae69587ce0c1a106e6c
Dmitrii <Simasorokin@gmail.com>
2018-03-09 01:09:19 +0300

    The first commit

---
+++ UC1/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+    public static void DataArt(int a,int b, int c) {
+        // write your code here
+        a = 5;
+        b=4;
+        c=a+b;
+        System.out.println(c);
+    }
+}
