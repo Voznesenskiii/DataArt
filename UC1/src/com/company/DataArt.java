808e69da39716b63963d8d03db7eb772076195f0
Dmitrii <Simasorokin@gmail.com>
2018-04-19 23:04:57 +0300

    The first commit

--- UC1/src/com/company/DataArt.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-import javax.swing.*;
-
-public class DataArt {
-    private JButton rootPanel;
-}
--- UC1/src/com/company/Main.java
+++ UC1/src/com/company/Main.java
@@ -1,17 +1,45 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введи буквы: ");
+        String confession = in.nextLine();
+        int count = getCount(confession);
+        System.out.print("Кол-во букв: "+ count);
+
+
 
     }
 
-    public static void DataArt(int a,int b, int c) {
-        // write your code here
-        a = 5;
-        b=4;
-        c=a+b;
-        System.out.println(c);
+
+    public static int getCount (String str) {
+        int vowelsCount = 0;
+
+        char a = 'a';
+        char e = 'e';
+        char i = 'i';
+        char o = 'o';
+        char u = 'u';
+
+        //char[] cats = {'a', 'e', 'i', 'o', 'u'};
+
+        for (int z=0; z <str.length(); z++ ){
+            if (str.charAt(z)==a||str.charAt(z)==e||str.charAt(z)==i||str.charAt(z)==o||str.charAt(z)==u){
+            //if (str.charAt(z)==cats){
+                vowelsCount = vowelsCount + 1;
+            }
+            else {
+                System.out.print("");
+            }
+        }
+
+        return  vowelsCount;
     }
+
+
 }
+
+
