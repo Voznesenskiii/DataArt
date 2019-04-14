c16eaa47b69eaaecbee6c4c1f8eb2f75931424d8
Dmitrii <Simasorokin@gmail.com>
2019-04-14 20:52:44 +0300

    DataArt

---
+++ src/DataArt/CosCalculator.java
@@ -0,0 +1,28 @@
+package DataArt;
+
+/*
+cos x = 1 - x^2/2! + x^4/4! + x^6/6! +...
+ */
+
+public class CosCalculator {
+
+    public static int pow(int x, int exponent){
+        int res=1;
+        for (int i = 0; i<exponent; i++){
+            res *=x;
+        }
+        return res;
+    }
+
+    public static int factorial(int n) {
+
+        for (){
+
+        }
+
+        return n;
+    }
+
+
+
+}
---
+++ src/DataArt/Main.java
@@ -0,0 +1,8 @@
+package DataArt;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = CosCalculator.pow(3, 5);
+        System.out.println(result);
+    }
+}
